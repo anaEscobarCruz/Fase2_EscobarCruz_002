4d6e95fbba03f192dc1f724c34bb1854d8eaf5a5
anaEscobarCruz <anl.escobar@alumnos.duoc.cl>
2020-10-30 04:42:26 -0300

    Creando modelos

--- articulos/catalogo/models.py
+++ articulos/catalogo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.urls import reverse
+import uuid
+class Producto(models.Model):
+    id=models.IntegerField(primary_key=True, help_text="Código del producto")
+    nom=models.CharField(max_length=200)
+    cant=models.IntegerField()
+    def __str__(self):
+        return self.nom
