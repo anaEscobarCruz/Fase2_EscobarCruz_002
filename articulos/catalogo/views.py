c421237929f884e7e64793671d15822819df0780
anaEscobarCruz <anl.escobar@alumnos.duoc.cl>
2020-11-01 19:25:16 -0300

    Creando Página de Inicio

--- articulos/catalogo/views.py
+++ articulos/catalogo/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from . models import Producto
+from django.views import generic
 
 # Create your views here.
+def Index(request) :
+    produ = Producto.objects.all().count()
+
+    return render(
+        request,
+        'index.html',
+        context={'produ': produ},
+    )
