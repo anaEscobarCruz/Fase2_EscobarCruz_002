2f5da81078b80fddef218072d5d94613a5271829
anaEscobarCruz <anl.escobar@alumnos.duoc.cl>
2020-11-02 02:03:04 -0300

    Creando Formularios

--- articulos/catalogo/urls.py
+++ articulos/catalogo/urls.py
@@ -2,4 +2,11 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('',views.Index,name='Index'),
+    path('producto/<int:pk>',views.ProdDetailView.as_view(), name='prod_detail'),
+]
+
+urlpatterns+=[
+    path('producto/create/', views.ProdCreate.as_view(), name='prod_create'),
+    path('producto/<int:pk>/update/', views.ProdUpdate.as_view(), name='prod_update'),
+    path('producto/<int:pk>/delete/', views.ProdDelete.as_view(), name='prod_delete'),
 ]
--- articulos/catalogo/views.py
+++ articulos/catalogo/views.py
@@ -11,3 +11,23 @@ def Index(request) :
         'index.html',
         context={'produ': produ},
     )
+
+#Formulario
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Producto
+
+class ProdCreate(CreateView):
+    model = Producto
+    fields = ['nom', 'cant']
+
+class ProdUpdate(UpdateView):
+    model = Producto
+    fields = ['nom', 'cant']
+
+class ProdDelete(DeleteView):
+    model = Producto
+    sucess_url = reverse_lazy('productos')
+
+class ProdDetailView(generic.DetailView):
+    model = Producto
